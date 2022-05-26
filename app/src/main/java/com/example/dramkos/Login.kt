4995bdb7f49c610212c6fa94932c28991f4942f9
aininura <95525366+aininura@users.noreply.github.com>
2022-05-26 21:46:41 +0700

    aini(coba)
    1. register
    2. register admit
    3. register user
    4. login
    5. tombol navigasi

--- app/src/main/java/com/example/dramkos/Login.kt
+++ app/src/main/java/com/example/dramkos/Login.kt
@@ -1,11 +1,11 @@
-package com.example.myapplication
+package com.example.dramkos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class tampilanlogin : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tampilanlogin)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/example/dramkos/RegisterUser.kt
@@ -0,0 +1,11 @@
+package com.example.dramkos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user)
+    }
+}
